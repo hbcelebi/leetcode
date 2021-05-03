07b69c08ccf926984fbd8d1d469cf5a865cd7bf7
Hasan Basri Celebi <celebihbasri@gmail.com>
2021-05-03 14:57:22 +0200

    Update main.py

--- #283_Move_Zeros/main.py
+++ #283_Move_Zeros/main.py
@@ -1,26 +1,31 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
+
 """
 Created on Mon May  3 14:40:08 2021
 
 @author: hbc
 """
 
+# This is a O(N) computational complexity and O(1) space complexity solution to the problem
 from typing import List
 
+### Here starts my code
 class Solution(object):
     def moveZeroes(self, nums: List[int]) -> None:
         """
         :type nums: List[int]
         :rtype: None Do not return anything, modify nums in-place instead.
         """
-        j = 0
+        # initialize the index
+        idx = 0
+        
+        # search through the array and move the nonzeros to the beginning of the array
         for num in nums:
             if(num != 0):
-                nums[j] = num
-                j += 1
-
-        for x in range(j, len(nums)):
+                nums[idx] = num
+                idx += 1
+        
+        # add zeros at the end of the array
+        for x in range(idx, len(nums)):
             nums[x] = 0
             
         print([nums])
